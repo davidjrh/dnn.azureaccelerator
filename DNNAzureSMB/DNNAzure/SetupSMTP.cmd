ba74f25a51f2c9dc4be7b52aea49b0d138a277fd
davidjrh_cp <SND\davidjrh_cp>
2012-05-31 11:27:00 +0000

    - Added a startup task to setup and configure the localhost SMTP server on the webroles

---
+++ DNNAzureSMB/DNNAzure/SetupSMTP.cmd
@@ -0,0 +1,7 @@
+﻿if defined AZURE_CLOUD_SIMULATION_ENVIRONMENT goto SKIP
+
+reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /d Unrestricted /f
+powershell .\SetupSMTP.ps1
+cscript ConfigSMTP.vbs
+
+SKIP:
