964e414711ae8eaacf49b46aa344d1e1caba7533
davidjrh_cp <SND\davidjrh_cp>
2012-11-06 22:31:35 +0000

    - Solution Upgraded to Azure SDK 1.8 (October 2012) - Changed the OSVersion=3 (Windows Server 2012)

--- DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
+++ DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
@@ -3,6 +3,6 @@ if not "%ENABLED%" == "true" goto SKIP
 
 start /w ocsetup IIS-ManagementService  
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WebManagement\Server /v EnableRemoteManagement /t REG_DWORD /d 1 /f  
-net start wmsvc  
+net start wmsvc >> SetupIISRemoteMgmt_log.txt 2>> SetupIISRemoteMgmt_err.txt
 
 :SKIP
