919993aa36a25440de52b3e50f597d4f1de1502e
David Rodríguez <davidjrh@hotmail.com>
2013-02-19 17:53:54 +0000

    Merged "High Availability" branch

--- DNNAzureSMB/DNNShared/Properties/AssemblyInfo.cs
+++ DNNAzureSMB/DNNShared/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.3.0.0")]
-[assembly: AssemblyFileVersion("6.3.0.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
