0ae2a33e7ee640f19b06e6535a22307b5db6d947
davidjrh_cp <SND\davidjrh_cp>
2013-02-19 17:53:54 +0000

    Merged "High Availability" branch

--- Trunk/DNNAzureSMB/DNNAzureWizard/Properties/AssemblyInfo.cs
+++ Trunk/DNNAzureSMB/DNNAzureWizard/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.3.*")]
+[assembly: AssemblyVersion("7.0.*")]
 //[assembly: AssemblyFileVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("6.3 RTM")]
+[assembly: AssemblyInformationalVersion("7.0 BETA")]
