7c9123dd6a73e9eb7d0c01d0f9ef282ed7988c90
davidjrh_cp <SND\davidjrh_cp>
2013-03-24 18:26:24 +0000

    - Changed the status from BETA to RTM

--- Trunk/DNNAzureSMB/DNNAzureWizard/Properties/AssemblyInfo.cs
+++ Trunk/DNNAzureSMB/DNNAzureWizard/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.4.*")]
 //[assembly: AssemblyFileVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("6.4 BETA")]
+[assembly: AssemblyInformationalVersion("6.4 RTM")]
