6b6ec422b6d4d5429f2b94c90cbf12729b473028
David Rodríguez <davidjrh@hotmail.com>
2013-05-22 21:25:37 +0000

    Added a fix to include the databaseOwner and objectQualifier settings while creating the portal aliases for the Offline site

---
+++ DNNAzureSMB/DNNShared/Utils/SqlConnectionSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace DNNShared.Utils
+{
+    public class SqlConnectionSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseOwner { get; set; }
+        public string ObjectQualifier { get; set; }
+
+        public SqlConnectionSettings()
+        {
+            DatabaseOwner = "dbo";
+        }
+    }
+}
