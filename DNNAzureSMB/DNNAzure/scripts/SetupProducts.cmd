ac56c70b90aa0d3fea3bb16bd15260f266c10532
rleonr_cp <ricardoleon@users.noreply.github.com>
2013-06-04 15:32:27 +0000

    Support for Web Platform Installer custom feeds

--- DNNAzureSMB/DNNAzure/scripts/SetupProducts.cmd
+++ DNNAzureSMB/DNNAzure/scripts/SetupProducts.cmd
@@ -6,7 +6,15 @@ cd %ROLEROOT%\APPROOT\bin\scripts
 md %ROLEROOT%\APPROOT\bin\scripts\AppData 
 
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%ROLEROOT%\APPROOT\bin\scripts\AppData" /f
+
+if "%CUSTOMFEED%" == "" goto NOCUSTOMFEED
+WebpiCmd.exe /Install /Products:%PRODUCTS% /Feeds:%CUSTOMFEED% /AcceptEula /Log:wpiinstalllog.txt
+goto ENDCUSTOMFEED
+
+:NOCUSTOMFEED
 WebpiCmd.exe /Install /Products:%PRODUCTS% /AcceptEula /Log:wpiinstalllog.txt
+
+:ENDCUSTOMFEED
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d %%USERPROFILE%%\AppData\Local /f
 
 :SKIP
