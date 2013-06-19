9555c7c99ab6ad25fbd421c5cafb7c4c0c7e38e7
David Rodríguez <davidjrh@hotmail.com>
2013-06-19 14:55:10 +0000

    Shorten symbolic link path length

---
+++ DNNAzureSMB/DNNAzure/scripts/SetupSiteRoot.cmd
@@ -0,0 +1,11 @@
+﻿if "%EMULATED%" == "true" goto SKIP
+
+REM Enable short names on volume C:
+fsutil.exe 8dot3name set c: 0
+
+REM Add a short name to the local storage folder
+fsutil.exe file setshortname "C:\Resources\Directory\%RoleDeploymentID%.DNNAzure.SitesRoot" Sites
+
+:SKIP
+
+EXIT /B 0
