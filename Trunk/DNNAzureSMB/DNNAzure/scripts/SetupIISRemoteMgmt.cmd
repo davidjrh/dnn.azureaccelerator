bfb313104809ba8d40a0f9b035e2ae26a6587bd9
davidjrh_cp <SND\davidjrh_cp>
2013-10-20 22:03:28 +0000

    OS Family 4 support (Windows Server 2012 R2)

--- Trunk/DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
+++ Trunk/DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
@@ -1,9 +1,12 @@
 if "%EMULATED%" == "true" goto SKIP
 if not "%ENABLED%" == "true" goto SKIP
 
-start /w ocsetup IIS-ManagementService  
+dism /online /enable-feature /featurename:IIS-WebServerRole 
+dism /online /enable-feature /featurename:IIS-WebServerManagementTools
+dism /online /enable-feature /featurename:IIS-ManagementService
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WebManagement\Server /v EnableRemoteManagement /t REG_DWORD /d 1 /f  
 net start wmsvc >> SetupIISRemoteMgmt_log.txt 2>> SetupIISRemoteMgmt_err.txt
+sc config wmsvc start=auto
 
 :SKIP
 
