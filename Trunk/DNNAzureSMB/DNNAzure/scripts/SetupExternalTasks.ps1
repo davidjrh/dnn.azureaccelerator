af55882b2554d208ce1d075dd0b8283ec914296a
davidjrh_cp <SND\davidjrh_cp>
2013-12-16 22:03:18 +0000

    Added support for multiple external startup tasks files

--- Trunk/DNNAzureSMB/DNNAzure/scripts/SetupExternalTasks.ps1
+++ Trunk/DNNAzureSMB/DNNAzure/scripts/SetupExternalTasks.ps1
@@ -1,7 +1,8 @@
-﻿ param (
+﻿param (
     [string]$tasksUrl = $(throw "-taskUrl is required."),
     [string]$localFolder = ""
- )
+)
+
 
 # Function to unzip file contents
 function Expand-ZIPFile($file, $destination)
@@ -22,59 +23,75 @@ function Write-Log($message) {
     Add-Content $localfolder\SetupExternalTasks.log $content
 }
 
- if ($tasksUrl -eq "") {
+
+# Function to process an external task Url
+function Process-ExternalTask($taskUrl, $localFolder, $index) {
+    try {
+        # Define local filename
+        $file = "$localFolder\ExternalTasks_$index.cmd"
+        if ($taskUrl.ToLower().EndsWith(".zip")) {
+            $file = "$localFolder\ExternalTasks_$index.zip"
+        }
+        if ($taskUrl.ToLower().EndsWith(".ps1")) {
+            $file = "$localFolder\ExternalTasks_$index.ps1"
+        }
+
+        # Download the tasks file
+        Write-Log "Downloading $taskUrl to file $file..."
+        $webclient = New-Object System.Net.WebClient
+        $webclient.DownloadFile($taskUrl, $file)
+        Write-Log "Download completed"
+
+        # If the tasks are zipped, unzip them first
+        if ($taskUrl.ToLower().EndsWith(".zip")) {
+            Write-Log "Unzipping $file..."
+            Expand-ZIPFile -file "$file" -destination $localFolder
+            Write-Log "Unzip completed"
+
+            # When a .zip file is specied, only files called "Task???.cmd" and "Task???.ps1" will be executed
+            # This allows to include assemblies and other file dependencies in the zip file
+            Get-ChildItem $localFolder | Where-Object {$_.Name.ToLower() -match "task[0-9][0-9][0-9].[cmd|ps1]"} | Sort-Object $_.Name | ForEach-Object {
+                Write-Log "Executing $localfolder\$_"        
+                if ($_.Name.ToLower().EndsWith(".ps1")) {
+                    powershell.exe "$localFolder\$_"
+                }
+                elseif ($_.Name.ToLower().EndsWith(".cmd")) {
+                    cmd.exe /C "$localFolder\$_"
+                }
+            }
+        }
+        elseif ($taskUrl.ToLower().EndsWith(".ps1")) {
+            powershell.exe $file
+        }
+        elseif ($taskUrl.ToLower().EndsWith(".cmd")) {
+            cmd.exe /C $file
+        }
+    }
+    catch {
+        Write-Log "Error while processing task $i : " + $_.Exception.Message
+    }
+}
+
+
+# MAIN =========================================
+if ($tasksUrl -eq "") {
     exit
- }
+}
 
 if ($localFolder -eq "") {
     $localFolder = "$pwd\External"
 }
 
 # Create folder if does not exist
-Write-Log "Creating folder $localFolder"
 New-Item -ItemType Directory -Force -Path $localFolder
 cd $localFolder
 
+# Split the URL in individual startup tasks
+$tasks = $tasksUrl.Split(";",[System.StringSplitOptions]::RemoveEmptyEntries)
+$i = 0
+foreach ($task in $tasks) {
+    Process-ExternalTask $task.Trim() $localFolder $i
+    $i += 1
+}
 
-$file = "$localFolder\ExternalTasks.cmd"
-
- if ($tasksUrl.ToLower().EndsWith(".zip")) {
-    $file = "$localFolder\ExternalTasks.zip"
- }
- if ($tasksUrl.ToLower().EndsWith(".ps1")) {
-    $file = "$localFolder\ExternalTasks.ps1"
- }
-
-# Download the tasks file
-Write-Log "Downloading external file $file"
-$webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile($tasksUrl,$file)
-
-Write-Log "Download completed"
-
-# If the tasks are zipped, unzip them first
- if ($tasksUrl.ToLower().EndsWith(".zip")) {
-    Write-Log "Unzipping $localFolder\ExternalTasks.zip"
-    Expand-ZIPFile -file "$localFolder\ExternalTasks.zip" -destination $localFolder
-    Write-Log "Unzip completed"
-
-    # When a .zip file is specied, only files called "Task???.cmd" and "Task???.ps1" will be executed
-    # This allows to include assemblies and other file dependencies in the zip file
-    Get-ChildItem $localFolder | Where-Object {$_.Name.ToLower() -match "task[0-9][0-9][0-9].[cmd|ps1]"} | Sort-Object $_.Name | ForEach-Object {
-        Write-Log "Executing $localfolder\$_"        
-        if ($_.Name.ToLower().EndsWith(".ps1")) {
-            powershell.exe "$localFolder\$_"
-        }
-        elseif ($_.Name.ToLower().EndsWith(".cmd")) {
-            cmd.exe /C "$localFolder\$_"
-        }
-    }
- }
- elseif ($tasksUrl.ToLower().EndsWith(".ps1")) {
-    powershell.exe $file
- }
- elseif ($tasksUrl.ToLower().EndsWith(".cmd")) {
-    cmd.exe /C $file
- }
-
- Write-Log "External tasks execution finished"
+Write-Log "External tasks execution finished"
