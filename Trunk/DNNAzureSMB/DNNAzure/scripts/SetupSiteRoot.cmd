23456a19e08c60e5268bd29fffc8a5cedc86d380
cesarab_cp <SND\cesarab_cp>
2014-05-02 18:58:14 +0000

    Added PowerShell script to create IIS log folders

--- Trunk/DNNAzureSMB/DNNAzure/scripts/SetupSiteRoot.cmd
+++ Trunk/DNNAzureSMB/DNNAzure/scripts/SetupSiteRoot.cmd
@@ -6,6 +6,9 @@ fsutil.exe 8dot3name set c: 0
 REM Add a short name to the local storage folder
 fsutil.exe file setshortname "C:\Resources\Directory\%RoleDeploymentID%.DNNAzure.SitesRoot" Sites
 
+reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /d Unrestricted /f
+powershell .\CreateIISLogFolders.ps1 >> CreateIISLog.log 2>> CreateIISLog_err.log
+
 :SKIP
 
 EXIT /B 0
