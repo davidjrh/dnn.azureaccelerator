5bdc7b433058a0d27829ac1541b328c46d63f75c
David Rodríguez <davidjrh@hotmail.com>
2014-12-29 16:08:08 +0000

    Added PowerShell remote management support

--- DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
+++ DNNAzureSMB/DNNAzure/scripts/SetupIISRemoteMgmt.cmd
@@ -8,6 +8,11 @@ reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WebManagement\Server /v EnableRemo
 net start wmsvc >> SetupIISRemoteMgmt_log.txt 2>> SetupIISRemoteMgmt_err.txt
 sc config wmsvc start=auto
 
+cd %ROLEROOT%\APPROOT\bin\scripts
+reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /d Unrestricted /f
+powershell .\SetupRemoteMgmt.ps1 >> SetupRemoteMgmt.log 2>> SetupRemoteMgmt_err.log
+
+
 :SKIP
 
 EXIT /B 0
